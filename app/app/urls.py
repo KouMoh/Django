555270950096ba1c10bb60badea1e35692efb8f1
KouMoh <126945657+KouMoh@users.noreply.github.com>
2024-12-28 23:28:01 +0530

    Update urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('about/', views.about, name = 'about'),
     path('contact/', views.contact, name = 'contact'),
-    path('firstApp', include('firstApp.urls')),
+    path('firstApp/', include('firstApp.urls')),
 
     path("__reload__/", include("django_browser_reload.urls")),
 
